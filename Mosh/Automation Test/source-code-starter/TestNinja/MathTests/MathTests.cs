175510c3b132255a7851d07e64c62a8562c28b3c
Yifan Zhang <yf.eva.yifan@gmail.com>
2018-03-09 22:05:37 -0500

    nunit test changes

---
+++ Mosh/Automation Test/source-code-starter/TestNinja/MathTests/MathTests.cs
@@ -0,0 +1,40 @@
+﻿
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace MathTests
+{
+
+
+    [TestFixture]
+    public class MathTests
+    {
+        private TestNinja.Fundamentals.Math _math;
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnFirstArgument()
+        {
+            var results = _math.Max(2, 1);
+            Assert.That(results, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnSecondArgument()
+        {
+            var results = _math.Max(1, 2);
+            Assert.That(results, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_ArgumentsEqual_ReturnSame()
+        {
+            var results = _math.Max(1, 1);
+
+            Assert.That(results, Is.EqualTo(1));
+        }
+    }
+}
---
+++ Mosh/Automation Test/source-code-starter/TestNinja/ReservationTests/ReservationTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace ReservationTests
+{
+    [TestClass]
+    public class ReservationTest
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_isAdmin_ReturensTrue()
+        {
+            var reservation = new Reservation();
+
+            var results = reservation.CanBeCancelledBy(new User() { IsAdmin = true });
+
+            Assert.IsTrue(results);
+        }
+    }
+}
