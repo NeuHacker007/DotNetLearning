4a8e2c73bd1879c423af867f7b3315069ce976c2
Yifan Zhang <yf.eva.yifan@gmail.com>
2018-03-14 22:39:08 -0400

    using Moq library

--- Mosh/Automation Test/source-code-starter/TestNinja/TestNinja.Tests/Mocking/VideoServiceTests.cs
+++ Mosh/Automation Test/source-code-starter/TestNinja/TestNinja.Tests/Mocking/VideoServiceTests.cs
@@ -18,6 +18,7 @@
 *
 */
 
+using Moq;
 using NUnit.Framework;
 using TestNinja.Mocking;
 
@@ -27,10 +28,15 @@ namespace TestNinja.Tests
     class VideoServiceTests
     {
         private VideoService _vs;
+        private VideoService _videoService;
+        private Mock<IFileReader> _fileReader;
+
         [SetUp]
         public void SetUp()
         {
             _vs = new VideoService(new FileReaderForTest());
+            _fileReader = new Mock<IFileReader>();
+            _videoService = new VideoService(_fileReader.Object);
         }
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnErrorMsg()
@@ -38,6 +44,15 @@ namespace TestNinja.Tests
             //_vs.FileReader = new FileReaderForTest();
             var result = _vs.ReadVideoTitle();
 
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnError()
+        {
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
+
+            var result = _videoService.ReadVideoTitle();
             Assert.That(result, Does.Contain("error").IgnoreCase);
         }
     }
