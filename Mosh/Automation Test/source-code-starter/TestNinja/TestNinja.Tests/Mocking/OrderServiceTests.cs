6527a7bb7dc7b9e773bf11aa2ac240cb750dca6b
Yifan Zhang <yf.eva.yifan@gmail.com>
2018-03-15 09:49:52 -0400

    test orders using mock

---
+++ Mosh/Automation Test/source-code-starter/TestNinja/TestNinja.Tests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,43 @@
+﻿/**
+* @Project Name: $projectname$ ： OrderServiceTests
+* @Project Desc:
+*
+* @Machine Name: IVAN_ZHANG
+* @Namespace Name: TestNinja.Tests.Mocking
+* @Creation Time:  3/14/2018 10:44:59 PM
+* @Author Ivan
+* @Email  yf.eva.yifan@gmail.com
+*
+* @Change Log
+*
+* Version       Change Date               Changed By                  Changes
+* ───────────────────────────────────────────────────────────────────────────────
+* V0.01          3/14/2018 10:44:59 PM                    Ivan                  Initial
+*
+* Copyright (c) @ Yifan Zhang 2018.  All rights reserved.
+*
+*/
+
+
+using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.Tests.Mocking
+{
+    [TestFixture]
+    class OrderServiceTests
+    {
+
+        [Test]
+        public void PlaceOrder_WhenCalled_StoreTheOrder()
+        {
+            var storage = new Mock<IStorage>();
+            var service = new OrderService(storage.Object);
+            var order = new Order();
+            service.PlaceOrder(order);
+
+            storage.Verify(s => s.Store(order));
+        }
+    }
+}
