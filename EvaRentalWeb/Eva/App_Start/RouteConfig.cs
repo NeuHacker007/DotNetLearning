93321712d86fefde815abc5fec5a6099578dafad
Yifan Zhang <yf.eva.yifan@gmail.com>
2018-03-16 20:07:00 -0400

    normal customer routing in routing config

--- EvaRentalWeb/Eva/App_Start/RouteConfig.cs
+++ EvaRentalWeb/Eva/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Eva
@@ -12,7 +8,10 @@ namespace Eva
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapRoute(
+                name: "MovieReleasedByDate",
+                url: "movies/released/{year}/{month}",
+                defaults: new { Controller = "Movies", Action = "ByReleasedDate" });
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ EvaRentalWeb/Eva/Controllers/MoviesController.cs
@@ -0,0 +1,38 @@
+﻿using Eva.Models;
+using System.Web.Mvc;
+
+namespace Eva.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shrek!"
+            };
+            return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content($"id is {id}");
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content($"pageIndex={pageIndex}&&sortby={sortBy}");
+        }
+
+        public ActionResult ByReleasedDate(int year, int month)
+        {
+            return Content($"movies released at {year} - {month}");
+        }
+    }
+}
