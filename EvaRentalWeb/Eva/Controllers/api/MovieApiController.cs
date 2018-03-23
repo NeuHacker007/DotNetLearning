ebd3db31eb83388aab0fc42f92c59af05ab18f18
Yifan Zhang <yf.eva.yifan@gmail.com>
2018-03-22 21:55:00 -0400

    Initialed movie api

--- EvaRentalWeb/Eva/Controllers/api/MovieApiController.cs
+++ EvaRentalWeb/Eva/Controllers/api/MovieApiController.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using Eva.Models;
+using System;
+using System.Collections.Generic;
 using System.Linq;
-using Eva.Models;
+using System.Net;
 using System.Web.Http;
 
 namespace Eva.Controllers.api
@@ -18,6 +20,68 @@ namespace Eva.Controllers.api
         public IEnumerable<Movie> GetMovies()
         {
             return _context.Movies.ToList();
+        }
+
+        // GET /api/Movies/id
+        public Movie GetMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return movie;
+        }
+
+        // POST /api/Movies
+        [HttpPost]
+        public IHttpActionResult CreateMovie(Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+            return Created(new Uri(Request.RequestUri + "/" + movie.Id), movie);
+        }
+
+        //PUT /api/movie/id
+
+        [HttpPut]
+        public void UpdateMovie(int id, Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            var movieInDb = _context.Movies.SingleOrDefault(m => m.Id == id);
+            if (movieInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            movieInDb.Name = movie.Name;
+            movieInDb.GenreId = movie.GenreId;
+            movieInDb.ReleasedDate = movie.ReleasedDate;
+            movieInDb.NumberInStock = movie.NumberInStock;
+
+            _context.SaveChanges();
+        }
+
+
+        //DELETE /api/movies/id
+        public void DELETE(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
         }
     }
 }
