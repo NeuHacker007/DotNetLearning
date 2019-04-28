4d8034af430600e8184e15391f541c0cf90ab5fc
Yifan Zhang <yf.eva.yifan@gmail.com>
2019-04-27 22:18:45 -0400

    -- Add a default HTML but the it  cannot be see due to the pipeline haven't been created. -- Test the middleware order impact -- added the logging injection

--- NetCoreLearning/Projects/EmployeeManagementWeb/Startup.cs
+++ NetCoreLearning/Projects/EmployeeManagementWeb/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace EmployeeManagementWeb
 {
@@ -25,16 +26,22 @@ namespace EmployeeManagementWeb
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("MW1: Incoming Request");
+                await context.Response.WriteAsync("This is my 1st middleware");
+                await next();
+                logger.LogInformation("MW1: Outgoing Response");
+            });
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync(_config["MyKey"]);
+                await context.Response.WriteAsync("This is my 2nd middle ware");
             });
         }
     }
