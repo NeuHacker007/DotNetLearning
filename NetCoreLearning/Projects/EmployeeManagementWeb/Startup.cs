7534e98f7e9c2d69f3ad4e86194c7e4a001aae32
Yifan Zhang <yf.eva.yifan@gmail.com>
2019-04-29 20:53:12 -0400

    DI for .NET Core

--- NetCoreLearning/Projects/EmployeeManagementWeb/Startup.cs
+++ NetCoreLearning/Projects/EmployeeManagementWeb/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeeManagementWeb.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -24,6 +25,7 @@ namespace EmployeeManagementWeb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
