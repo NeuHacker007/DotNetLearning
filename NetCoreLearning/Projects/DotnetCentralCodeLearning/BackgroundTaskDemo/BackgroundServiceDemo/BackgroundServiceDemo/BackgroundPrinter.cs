1c96ab37b36eec8c9474f4a1c75a84c228569bb4
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-07-28 21:41:19 -0400

    BackgroundPrinter Updates -- Added the background services and make it print in background

---
+++ NetCoreLearning/Projects/DotnetCentralCodeLearning/BackgroundTaskDemo/BackgroundServiceDemo/BackgroundServiceDemo/BackgroundPrinter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace BackgroundServiceDemo
+{
+    public class BackgroundPrinter : IHostedService, IDisposable
+    {
+        private readonly ILogger<BackgroundPrinter> _logger;
+        private int _number = 0;
+
+        private Timer timer;
+
+        public BackgroundPrinter(ILogger<BackgroundPrinter> logger)
+        {
+            this._logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            timer = new Timer(t =>
+            {
+                Interlocked.Increment(ref _number);
+                _logger.LogInformation($"Printing from worker number: {_number}");
+            }, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Printing worker is stopped");
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            timer?.Dispose();
+        }
+    }
+}
--- NetCoreLearning/Projects/DotnetCentralCodeLearning/BackgroundTaskDemo/BackgroundServiceDemo/BackgroundServiceDemo/Program.cs
+++ NetCoreLearning/Projects/DotnetCentralCodeLearning/BackgroundTaskDemo/BackgroundServiceDemo/BackgroundServiceDemo/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -21,6 +22,7 @@ namespace BackgroundServiceDemo
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                }).ConfigureServices(services =>
+                    services.AddHostedService<BackgroundPrinter>());
     }
 }
