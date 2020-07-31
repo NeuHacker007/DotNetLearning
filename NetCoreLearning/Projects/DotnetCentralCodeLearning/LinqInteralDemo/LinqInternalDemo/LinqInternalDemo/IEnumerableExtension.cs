b535f05558650242d1cc4aa7770aab8a0f3fc3e4
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-07-30 21:17:56 -0400

    Dived into the linq where method and implement our own where method

---
+++ NetCoreLearning/Projects/DotnetCentralCodeLearning/LinqInteralDemo/LinqInternalDemo/LinqInternalDemo/IEnumerableExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqInternalDemo
+{
+    public static class EnumerableExtension
+    {
+        // this is an extension method.
+        public static IEnumerable<T> NewWhere<T>(this IEnumerable<T> items, Func<T, bool> predicateFunc)
+        {
+            foreach (var item in items)
+            {
+                if (predicateFunc(item))
+                {
+                    // here yield has the delay execution effects
+                    // only if the IEnumerable derived class call ToList/ ToArray or loop through,
+                    // the data will be filtered out.Otherwise, it just generate a query.
+                    yield return item;
+                }
+            }
+        }
+    }
+}
---
+++ NetCoreLearning/Projects/DotnetCentralCodeLearning/LinqInteralDemo/LinqInternalDemo/LinqInternalDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LinqInternalDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var even = items.NewWhere(x => x % 2 == 0);
+
+            foreach (var num in even)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
