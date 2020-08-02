aa9e664737a6cc6c2c05915c8a4ea06c0ad681b1
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-08-01 22:35:54 -0400

    Dive into the Global Error Handler

---
+++ NetCoreLearning/Projects/DotnetCentralCodeLearning/GlobalExceptionHandlerDemo/GlobalExceptionHandlerDemo/GlobalExceptionHandlerDemo/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalExceptionHandlerDemo.Controllers
+{
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [Route("/error")]
+        public IActionResult Error()
+        {
+            var exception = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            var statusCode = exception.Error.GetType().Name switch
+            {
+                "ArgumentException" => HttpStatusCode.BadRequest,
+                _ => HttpStatusCode.ServiceUnavailable
+            };
+            return Problem(detail: exception.Error.Message, statusCode: (int)statusCode);
+        }
+    }
+}
