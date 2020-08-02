d534adfd513952421075ab737f50316502507440
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-08-01 22:47:06 -0400

    use ExceptionHandlerOption -> ExceptionHandler delegate.

--- NetCoreLearning/Projects/DotnetCentralCodeLearning/GlobalExceptionHandlerDemo/GlobalExceptionHandlerDemo/GlobalExceptionHandlerDemo/Controllers/WeatherForecastController.cs
+++ NetCoreLearning/Projects/DotnetCentralCodeLearning/GlobalExceptionHandlerDemo/GlobalExceptionHandlerDemo/GlobalExceptionHandlerDemo/Controllers/WeatherForecastController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,7 +9,7 @@ namespace GlobalExceptionHandlerDemo.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        private static readonly string[] Summaries =
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
--- NetCoreLearning/Projects/DotnetCentralCodeLearning/GlobalExceptionHandlerDemo/GlobalExceptionHandlerDemo/GlobalExceptionHandlerDemo/Startup.cs
+++ NetCoreLearning/Projects/DotnetCentralCodeLearning/GlobalExceptionHandlerDemo/GlobalExceptionHandlerDemo/GlobalExceptionHandlerDemo/Startup.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace GlobalExceptionHandlerDemo
 {
@@ -31,18 +27,34 @@ namespace GlobalExceptionHandlerDemo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // app.UseExceptionHandler("/error");
 
-            app.UseExceptionHandler("/error");
+            app.UseExceptionHandler(new ExceptionHandlerOptions
+            {
+                // here ExceptionHandler is a delegate and it takes http context 
+                ExceptionHandler = c =>
+                {
+                    var exception = c.Features.Get<IExceptionHandlerFeature>();
+                    var statusCode = exception.Error.GetType().Name switch
+                    {
+                        "ArgumentException" => HttpStatusCode.BadRequest,
+                        _ => HttpStatusCode.ServiceUnavailable
+                    };
+                    c.Response.StatusCode = (int)statusCode;
+
+                    var content = Encoding.UTF8.GetBytes($"Error [{exception.Error.Message}]");
+                    c.Response.Body.WriteAsync(content, 0, content.Length);
+
+                    return Task.CompletedTask;
+                }
+            });
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
