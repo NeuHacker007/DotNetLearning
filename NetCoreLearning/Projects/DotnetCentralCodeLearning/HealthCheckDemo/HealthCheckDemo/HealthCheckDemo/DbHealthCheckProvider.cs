2ed4b61df2c86c08fd99e26f8fb5d5c46648e11f
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-08-07 16:42:52 -0400

    Dive into the healthy check in dotnet core

---
+++ NetCoreLearning/Projects/DotnetCentralCodeLearning/HealthCheckDemo/HealthCheckDemo/HealthCheckDemo/DbHealthCheckProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace HealthCheckDemo
+{
+    public static class DbHealthCheckProvider
+    {
+        public static HealthCheckResult Check(string connectionString)
+        {
+            // code to check if db is running 
+            return HealthCheckResult.Healthy();
+        }
+    }
+}
---
+++ NetCoreLearning/Projects/DotnetCentralCodeLearning/HealthCheckDemo/HealthCheckDemo/HealthCheckDemo/MqHealthCheckProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace HealthCheckDemo
+{
+    public static class MqHealthCheckProvider
+    {
+        public static HealthCheckResult Check(string mqUri)
+        {
+            // code to check if db is running 
+            return HealthCheckResult.Healthy();
+        }
+    }
+}
