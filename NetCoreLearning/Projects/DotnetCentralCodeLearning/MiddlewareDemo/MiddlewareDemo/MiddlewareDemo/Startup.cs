e9bfabc98be76a2bb1b38212fb3ed31ae23532b7
Yifan Zhang <yf.eva.yifan@gmail.com>
2020-08-10 21:57:17 -0400

    -- Initial demo project
    -- Dive into Run method

---
+++ NetCoreLearning/Projects/DotnetCentralCodeLearning/MiddlewareDemo/MiddlewareDemo/MiddlewareDemo/Startup.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MiddlewareDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async ctx =>
+            {
+                await ctx.Response.WriteAsync($"Response from 1st middleware");
+            });
+
+            app.Run(async ctx =>
+            {
+                await ctx.Response.WriteAsync($"Response from 2st middleware");
+            });
+        }
+    }
+}
