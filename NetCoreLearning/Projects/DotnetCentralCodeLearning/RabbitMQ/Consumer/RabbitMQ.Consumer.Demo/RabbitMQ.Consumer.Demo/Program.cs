78252914d2a319f10163e47c4968cf134166a6d3
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-03-05 16:20:00 -0500

    RabbitMQ -- Implemented the consumer

---
+++ NetCoreLearning/Projects/DotnetCentralCodeLearning/RabbitMQ/Consumer/RabbitMQ.Consumer.Demo/RabbitMQ.Consumer.Demo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ.Consumer.Demo
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+           var factory = new ConnectionFactory()
+           {
+               Uri = new Uri("amqp://guest:guest@localhost:5672")
+           };
+
+           using var connection = factory.CreateConnection();
+           using var channel = connection.CreateModel();
+
+           channel.QueueDeclare("demo-queue", true, false, false, null);
+
+           var consumer = new EventingBasicConsumer(channel);
+
+           consumer.Received += (sender, e) =>
+           {
+               var body = e.Body.ToArray();
+               var msg = Encoding.UTF8.GetString(body);
+               Console.WriteLine(msg);
+           };
+
+           channel.BasicConsume("demo-queue", true, consumer);
+
+           Console.ReadLine();
+        }
+    }
+}
