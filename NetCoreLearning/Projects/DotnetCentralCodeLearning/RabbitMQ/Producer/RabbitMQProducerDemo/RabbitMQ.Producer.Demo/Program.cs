3294fb6aa8f5d311564bf0601c864466494f9c5e
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-03-05 10:41:56 -0500

    RabbitMQ  Learning -- Created basic RabbitMQ Producer

---
+++ NetCoreLearning/Projects/DotnetCentralCodeLearning/RabbitMQ/Producer/RabbitMQProducerDemo/RabbitMQ.Producer.Demo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace RabbitMQ.Producer.Demo
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+
+            using var connection = factory.CreateConnection();
+
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("demo-queue", true, false, false, null);
+
+            var msg = new {Name = "Producer", Msg = "Hello"};
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(msg));
+
+            channel.BasicPublish("", "demo-queue",null, body);
+        }
+    }
+}
