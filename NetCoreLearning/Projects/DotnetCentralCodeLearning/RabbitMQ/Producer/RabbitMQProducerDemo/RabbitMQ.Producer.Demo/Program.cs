d23fb016c4d62b108dedd612d5a456ed21ad5c0c
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-03-08 21:35:27 -0500

    implemented the new queue producer

--- NetCoreLearning/Projects/DotnetCentralCodeLearning/RabbitMQ/Producer/RabbitMQProducerDemo/RabbitMQ.Producer.Demo/Program.cs
+++ NetCoreLearning/Projects/DotnetCentralCodeLearning/RabbitMQ/Producer/RabbitMQProducerDemo/RabbitMQ.Producer.Demo/Program.cs
@@ -17,12 +17,7 @@ namespace RabbitMQ.Producer.Demo
             using var connection = factory.CreateConnection();
 
             using var channel = connection.CreateModel();
-            channel.QueueDeclare("demo-queue", true, false, false, null);
-
-            var msg = new {Name = "Producer", Msg = "Hello"};
-            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(msg));
-
-            channel.BasicPublish("", "demo-queue",null, body);
+            QueueProducer.Publish(channel);
         }
     }
 }
---
+++ NetCoreLearning/Projects/DotnetCentralCodeLearning/RabbitMQ/Producer/RabbitMQProducerDemo/RabbitMQ.Producer.Demo/QueueProducer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace RabbitMQ.Producer.Demo
+{
+    public static class QueueProducer
+    {
+        public static void Publish(IModel channel)
+        {
+            channel.QueueDeclare("demo-queue", true, false, false, null);
+
+            var count = 0;
+
+            while (true)
+            {
+                var msg = new { Name = "Producer", Msg = $"Hello! Count: {count}" };
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(msg));
+
+                channel.BasicPublish("", "demo-queue", null, body);
+                count++;
+            }
+
+            
+        }
+    }
+}
