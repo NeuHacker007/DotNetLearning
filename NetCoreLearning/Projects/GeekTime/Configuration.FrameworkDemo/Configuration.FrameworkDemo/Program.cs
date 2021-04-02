14f2bf50d5ce2e460e2a87486385dffc68c3d79d
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-04-02 13:13:39 -0400

    Deleted un-used projects

---
+++ NetCoreLearning/Projects/GeekTime/Configuration.FrameworkDemo/Configuration.FrameworkDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Configuration.FrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
