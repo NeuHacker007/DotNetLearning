26c08e0d874f7c2b1c0feae16bbef5576e9b0b66
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-04-06 14:39:56 -0400

    learned reading configuration in the enviornment variable settings.

---
+++ NetCoreLearning/Projects/GeekTime/Configuration.FrameworkDemo/EnvironmentVariable.Demo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace EnvironmentVariable.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            //builder.AddEnvironmentVariables();
+
+            var config = builder.Build();
+            Console.WriteLine($"Key1: {config["key1"]}");
+
+            //Sectioned configuration
+
+            var section = config.GetSection("SECTION1");
+            Console.WriteLine($"Key3: {section["KEY3"]}");
+
+            var section2 = config.GetSection("SECTION1:SECTION2");
+            Console.WriteLine($"Key4: {section2["KEy4"]}");
+
+            //prefix pattern;
+
+            builder.AddEnvironmentVariables("Ivan_");
+            var config2 = builder.Build();
+            Console.WriteLine($"Ivan Key1: {config2["key1"]}");
+            Console.WriteLine($"Key3: {section["KEY3"]}");
+        }
+    }
+}
