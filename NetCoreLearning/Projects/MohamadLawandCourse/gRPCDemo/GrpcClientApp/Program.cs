2c3229208ce736171a17e89dd45e91bad3489013
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-07-23 14:36:04 -0400

    Implemented the gRPC client APP

---
+++ NetCoreLearning/Projects/MohamadLawandCourse/gRPCDemo/GrpcClientApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Grpc.Net.Client;
+using Grpc.Core;
+using System.Threading.Tasks;
+
+namespace GrpcClientApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello gRPC from Ivan!");
+
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+
+            var customerClient = new Customer.CustomerClient(channel);
+
+            var result = await customerClient.GetCustomerInfoAsync(new CustomerFindModel()
+            {
+                UserId = 10
+            });
+            // System.Console.WriteLine(result.FirstName);
+            System.Console.WriteLine($"First Name: {result.FirstName} \nLast Name: {result.LastName}");
+        }
+    }
+}
