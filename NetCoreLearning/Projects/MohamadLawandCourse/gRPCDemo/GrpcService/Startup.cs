bcf70813fd50dd294e995c651849e6a499dce96a
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-07-23 14:00:04 -0400

    Wired the CustomerService in the Startup

--- NetCoreLearning/Projects/MohamadLawandCourse/gRPCDemo/GrpcService/Startup.cs
+++ NetCoreLearning/Projects/MohamadLawandCourse/gRPCDemo/GrpcService/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GrpcService.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -32,6 +33,7 @@ namespace GrpcService
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<CustomerService>();
 
                 endpoints.MapGet("/", async context =>
                 {
