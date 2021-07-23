2e363a5bd09c560c80df1adeff329e5488c35ff6
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-07-23 08:41:39 -0400

    -added the upsert/delete/get all; -- added global readme; --updated the app.db;

--- NetCoreLearning/Projects/MohamadLawandCourse/RepositoryPatternDemo/PocketBook/Controllers/UsersController.cs
+++ NetCoreLearning/Projects/MohamadLawandCourse/RepositoryPatternDemo/PocketBook/Controllers/UsersController.cs
@@ -40,12 +40,44 @@ namespace PocketBook.Controllers
             return new JsonResult("Something went wrong") { StatusCode = 500 };
         }
 
+
         public async Task<IActionResult> GetItem(Guid id)
         {
             var user = await _unitOfWork.userRepository.GetById(id);
             if (user == null) return NotFound();
 
             return Ok(user);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var users = await _unitOfWork.userRepository.All();
+
+            return Ok(users);
+        }
+
+        [HttpPost("{id}")]
+        public async Task<IActionResult> UpdateItem(Guid id, User user)
+        {
+            if (id != user.Id) return BadRequest();
+            await _unitOfWork.userRepository.Upsert(user);
+            await _unitOfWork.CompleteAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItem(Guid id)
+        {
+            var item = await _unitOfWork.userRepository.GetById(id);
+
+            if (item == null) return BadRequest();
+
+            await _unitOfWork.userRepository.Delete(id);
+            await _unitOfWork.CompleteAsync();
+
+            return Ok(id);
         }
     }
 }
