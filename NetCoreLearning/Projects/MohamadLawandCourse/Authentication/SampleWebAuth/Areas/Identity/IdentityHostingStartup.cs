30ad394cd8dd42721b312d28c762932090990f38
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-07-23 22:08:19 -0400

    user asp.net code generator to generate the pages based on identity and our db context

---
+++ NetCoreLearning/Projects/MohamadLawandCourse/Authentication/SampleWebAuth/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SampleWebAuth.Data;
+using SampleWebAuth.Models;
+
+[assembly: HostingStartup(typeof(SampleWebAuth.Areas.Identity.IdentityHostingStartup))]
+namespace SampleWebAuth.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
