1cad6c6f1c96afd933d05bd027ce3ea45393d7e2
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-07-27 15:17:00 -0400

    restored the identity from the class library

--- NetCoreLearning/Projects/MohamadLawandCourse/MultiFactorAuthentication/MFADemo/Startup.cs
+++ NetCoreLearning/Projects/MohamadLawandCourse/MultiFactorAuthentication/MFADemo/Startup.cs
@@ -35,6 +35,7 @@ namespace MFADemo
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
