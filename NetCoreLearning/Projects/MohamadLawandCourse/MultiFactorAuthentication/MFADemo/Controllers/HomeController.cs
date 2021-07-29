633d8e50269aaf4182f4faaed294a36fda7c6d0d
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-07-29 08:27:09 -0400

    clean up code

--- NetCoreLearning/Projects/MohamadLawandCourse/MultiFactorAuthentication/MFADemo/Controllers/HomeController.cs
+++ NetCoreLearning/Projects/MohamadLawandCourse/MultiFactorAuthentication/MFADemo/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MFADemo.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using MFADemo.Models;
+using System.Diagnostics;
 
 namespace MFADemo.Controllers
 {
