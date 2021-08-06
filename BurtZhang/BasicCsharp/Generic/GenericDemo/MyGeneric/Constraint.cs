285bac5621599a3ac4a62776f8441973111f172b
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-08-06 10:08:12 -0400

    问题回答

--- BurtZhang/BasicCsharp/Generic/GenericDemo/MyGeneric/Constraint.cs
+++ BurtZhang/BasicCsharp/Generic/GenericDemo/MyGeneric/Constraint.cs
@@ -10,20 +10,33 @@ namespace MyGeneric
     {
         /// <summary>
         /// 泛型： 不同参数类型都能进来；任何类型都能进来，你知道我是谁？
-        /// 泛型约束 -- 基类约束: 保证T一定是people 或People的子类
+        /// 泛型约束 -- 基类约束: 保证T一定是people 或People的子类 （不能是sealed）
         /// 1. 可以使用基类里面的一切属性方法
         /// 2. 强制保证T一定是People或者People子类
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="tPram"></param>
         public static void Show<T>(T tPram) 
-            where T: People
+            where T: People//, ISports, IWork
         {
             Console.WriteLine($"{tPram.Id}_ {tPram.Name}");
             tPram.Hi();
+            //tPram.PingPang();
+            //tPram.Work();
         }
-        // 为啥不直接用基类
+        // 为啥不直接用基类： 约束可以叠加，更灵活
+        // 此实现与 line 19 - line24效果一样
+        // 但是使用泛型约束可以不止局限在People
+        // 可以让它既是people 或 people 子类， 也可以同时
+        // 是ISports, IWork
+        public static void ShowBase(People peop)
+        {
+            Console.WriteLine($"{peop.Id}_ {peop.Name}");
+            peop.Hi();
+        }
 
+        // 可以使用Int32来约束么？
+        // 不可以，用基类约束不能使用 sealed 的类
         public static T Get<T>(T t)
             //where T : ISports //接口约束, 一定是实现了该接口的
             //where T : class // 引用类型约束： 引用类型 （可以被设置为null的）
