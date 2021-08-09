7a88c55f2fef947b4f54b9adb8041b6841e765fb
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-08-09 14:36:29 -0400

    多播委托应用

---
+++ BurtZhang/BasicCsharp/DelegateEvent/DelegateEventDemo/MyDelegateEvent/Event/Cat.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDelegateEvent.Event
+{
+    /// <summary>
+    /// 直接调用别人实例的别的方法
+    /// 不管是增减/顺序 都会让猫不稳定
+    /// </summary>
+    public class Cat
+    {
+        public void Miao()
+        {
+            Console.WriteLine("{0} Miao", this.GetType().Name);
+            {
+                // 每增加或减少都需要改这个类
+                new Dog().Wang();
+                new Mouse().Run();
+                new Baby().Cry();
+                new Mother().Wispher();
+                new Father().Roar();
+                new Neighbor().Awake();
+                new Stealer().Hide();
+            }
+      
+        }
+
+        // 猫叫一声， 触发一系列后续动作
+        // 多了个 指定动作 正是这个不稳定 封装出去 甩锅
+
+        public MiaoDelegate miaoDelegateHandler;
+        public void MiaoNew()
+        { 
+            
+            Console.WriteLine("{0} MiaoNew", this.GetType().Name);
+            miaoDelegateHandler?.Invoke();
+        }
+    }
+
+    public delegate void MiaoDelegate();
+}
--- BurtZhang/BasicCsharp/DelegateEvent/DelegateEventDemo/MyDelegateEvent/Program.cs
+++ BurtZhang/BasicCsharp/DelegateEvent/DelegateEventDemo/MyDelegateEvent/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using MyDelegateEvent.DelegateExtend;
+using MyDelegateEvent.Event;
 
 namespace MyDelegateEvent
 {
@@ -20,6 +21,26 @@ namespace MyDelegateEvent
                 {
                     ListExtend test = new ListExtend();
                     test.Show();
+                }
+
+                {
+                    Console.WriteLine("*************************Event********************");
+                    {
+                        Cat cat = new Cat();
+                        cat.Miao();
+                    }
+                    {
+                        Cat cat = new Cat();
+
+                        cat.miaoDelegateHandler += new MiaoDelegate(new Mouse().Run);
+                        cat.miaoDelegateHandler += new MiaoDelegate(new Baby().Cry);
+                        cat.miaoDelegateHandler += new MiaoDelegate(new Mother().Wispher);
+                        cat.miaoDelegateHandler += new MiaoDelegate(new Father().Roar);
+                        cat.miaoDelegateHandler += new MiaoDelegate(new Neighbor().Awake);
+                        cat.miaoDelegateHandler += new MiaoDelegate(new Stealer().Hide);
+                        cat.MiaoNew();
+
+                    }
                 }
 
             }
