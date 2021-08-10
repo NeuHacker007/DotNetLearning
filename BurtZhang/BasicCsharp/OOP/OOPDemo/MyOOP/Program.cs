a2770eb8fa2c4ed86fc9e9f47c275d53d77eea47
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-08-10 09:48:30 -0400

    initial OOP code

---
+++ BurtZhang/BasicCsharp/OOP/OOPDemo/MyOOP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("***************OOP***********************");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+               // throw;
+            }
+        }
+    }
+}
