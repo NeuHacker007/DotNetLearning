6babd7dc83f3a94f9f6da26d9e21b5fedbd026ea
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-08-17 13:43:08 -0400

    linq

--- BurtZhang/BasicCsharp/LambdaLinq/LambdaLinqDemo/MyLinq/ExtendMethod.cs
+++ BurtZhang/BasicCsharp/LambdaLinq/LambdaLinqDemo/MyLinq/ExtendMethod.cs
@@ -9,6 +9,70 @@ namespace MyLinq
     public static class ExtendMethod
     {
 
+        /// <summary>
+        /// 加迭代器，按需获取(延迟获取)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="tList"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> GIenumerableWhere<T>(this IEnumerable<T> tList, Func<T, bool> func)
+        {
+          //  var list = new List<T>();
+
+            foreach (var t in tList)  
+            {
+                if (func.Invoke(t))
+                {
+                    yield return t;
+                }
+            }
+            
+        }
+
+        /// <summary>
+        /// 泛型，应对各种类型
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="tList"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public static List<T> GenericWhere<T>(this List<T> tList, Func<T, bool> func)
+        {
+            var list = new List<T>();
+
+            foreach (var t in tList)  
+            {
+                if (func.Invoke(t))
+                {
+                    list.Add(t);
+                }
+            }
+
+            return list;
+        }
+        
+        /// <summary>
+        /// 基于委托封装解耦，去掉重复代码，完成通用代码
+        /// </summary>
+        /// <param name="students"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public static List<Student> IvanWhere(this List<Student> students, Func<Student, bool> func)
+        {
+            var list = new List<Student>();
+
+            foreach (var student in students)  
+            {
+                if (func.Invoke(student))
+                {
+                    list.Add(student);
+                }
+            }
+
+            return list;
+        }
+
         //可以使用泛型写扩展方法，但是最好加上约束
         //否则等同于为所有类型添加该方法
         public static void Show<T> (this T t) where T : Student
---
+++ BurtZhang/BasicCsharp/LambdaLinq/LambdaLinqDemo/MyLinq/LinqShow.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLinq
+{
+    public class LinqShow
+    {
+        private List<Student> getStudents()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student()
+                {
+                    Id = 1,
+                    Name = "Ivan",
+                    ClassId = 2,
+                    Age = 35
+                },
+                new Student()
+                {
+                    Id = 1,
+                    Name = "Alpha",
+                    ClassId = 2,
+                    Age = 23
+                },
+                new Student()
+                {
+                    Id = 1,
+                    Name = "Hao",
+                    ClassId = 2,
+                    Age = 25
+                },
+                new Student()
+                {
+                    Id = 1,
+                    Name = "Eric",
+                    ClassId = 2,
+                    Age = 27
+                },
+                new Student()
+                {
+                    Id = 1,
+                    Name = "Eva",
+                    ClassId = 2,
+                    Age = 4
+                }
+            };
+
+            return students;
+        }
+
+
+        public void Show()
+        {
+            var students = this.getStudents();
+
+            {
+                var list = new List<Student>();
+                //没有扩展方法和delegate的方式过滤数据
+                foreach (var student in students)
+                {
+                    if (student.Age > 10)
+                    {
+                        list.Add(student);
+                    }
+                }
+            }
+
+            {
+                students.IvanWhere(i => i.Age > 30);
+            }
+
+            {
+                new List<int>() {1, 3, 4, 5, 612, 534, 2345, 1234, 6, 453, 45423, 4, 234, 5}.GenericWhere(g => g > 50);
+
+            }
+            {
+                students.GIenumerableWhere(i => i.Age > 30);
+
+            }
+        }
+    }
+}
