9c7943d6324ab6c71731985d609144ec51f5f6f9
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-08-18 14:44:01 -0400

    表达式目录树

--- BurtZhang/BasicCsharp/Expression/ExpressionDemo/ExpressionDemo/ExpressionTest.cs
+++ BurtZhang/BasicCsharp/Expression/ExpressionDemo/ExpressionDemo/ExpressionTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,7 +30,69 @@ namespace ExpressionDemo
                 Console.WriteLine();
             }
             {
+                //lambda 写法
+                Expression<Func<int, int, int>> exp = (m, n) => m * n + 2;
+                // 不用lambda的写法
 
+                ParameterExpression parameterExpression = Expression.Parameter(typeof(int), "m");
+                ParameterExpression parameterExpression2 = Expression.Parameter(typeof(int), "n");
+
+                BinaryExpression multiply = Expression.Multiply(parameterExpression, parameterExpression2);
+                ConstantExpression constant = Expression.Constant(2, typeof(int));
+                BinaryExpression add = Expression.Add(multiply, constant);
+
+                Expression<Func<int, int, int>> expression = Expression.Lambda<Func<int, int, int>>(
+                    add,
+                    new ParameterExpression[]
+                    {
+                        parameterExpression,
+                        parameterExpression2
+                    });
+            }
+
+            {
+                ConstantExpression conLeft = Expression.Constant(345);
+                ConstantExpression conRight = Expression.Constant(234);
+
+                BinaryExpression add = Expression.Add(conLeft, conRight);
+
+                Expression<Action> actExpression = Expression.Lambda<Action>(add, null); // ()=>345 + 456;
+                actExpression.Compile()();
+            }
+            {
+                Expression<Func<People, bool>> lambda = x => x.Id.ToString().Equals("5");
+
+                ParameterExpression parameterExpression = Expression.Parameter(typeof(People), "x");
+                var field = Expression.Field(parameterExpression, typeof(People).GetField("Id"));
+                var toString = typeof(People).GetMethod("ToString");
+                var equals = typeof(People).GetMethod("Equals");
+
+                var body = Expression.Call(
+                    Expression.Call(
+                        field,
+                        toString,
+                        new Expression[0]),
+                    equals,
+                    new Expression[]
+                    {
+                        Expression.Constant("5", typeof(string))
+                    });
+                
+                Expression<Func<People, bool>> expression = Expression.Lambda<Func<People, bool>>(
+                        body,
+                        new ParameterExpression[]
+                        {
+                            parameterExpression
+                        }
+
+                        );
+
+               var result = expression.Compile()(new People()
+                {
+                    Id = 5
+                });
+
+               Console.WriteLine(result);
             }
         }
     }
---
+++ BurtZhang/BasicCsharp/Expression/ExpressionDemo/ExpressionDemo/People.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionDemo
+{
+    public class People
+    {
+        public int Id { get; set; }
+    }
+}
