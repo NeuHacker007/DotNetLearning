ef38d9bd3db569da1739db0fb6b5d45de7c6cc2c
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-08-19 10:57:50 -0400

    使用visitor访问expression tree做出更改

---
+++ BurtZhang/BasicCsharp/Expression/ExpressionDemo/ExpressionDemo/ExpressionVisitorTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using ExpressionDemo.Visitor;
+
+namespace ExpressionDemo
+{
+    public class ExpressionVisitorTest
+    {
+        private static int Get(int k)
+        {
+            return k;
+        }
+        public static void Show()
+        {
+            {
+                Expression<Func<int, int, int>> exp = (x, y) => x * y + 2;
+
+                // 此时需求更改，需要将 x*y + 2 变成 x*y -2， 首先需要解析上面的expression 需要用到ExpressionVisitor类
+                OperationVisitor visitor = new OperationVisitor();
+
+                Expression expNew = visitor.Modify(exp);
+            }
+
+            {
+                Expression<Func<int, int, int>> exp1 = (m, n) => m * n + 2 + 3 + Get(m);
+                OperationVisitor visitor = new OperationVisitor();
+
+                Expression expNew = visitor.Modify(exp1);
+            }
+
+
+        }
+    }
+}
---
+++ BurtZhang/BasicCsharp/Expression/ExpressionDemo/ExpressionDemo/Visitor/OperationVisitor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionDemo.Visitor
+{
+    /// <summary>
+    /// ExpressionVisitor: 肯定需要递归解析表达式目录树，因为不知道深度的一棵树
+    /// 只有一个入口叫Visit
+    /// 首先检查是什么类型的表达式
+    /// 然后调通对应的protected virtual visit 方法
+    /// 得到结果继续去 检查类型 -- 调用对应的visit 方法 -- 再检测--再调用 （递归）
+    /// </summary>
+    public class OperationVisitor : ExpressionVisitor
+    {
+        public Expression Modify(Expression expression)
+        {
+           return base.Visit(expression);
+        }
+
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            if (node.NodeType == ExpressionType.Add)
+            {
+                Expression left = this.Visit(node.Left);
+                Expression right = this.Visit(node.Right);
+
+                return Expression.Subtract(left, right);
+            }
+
+            return base.VisitBinary(node);
+        }
+
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            return base.VisitMethodCall(node);
+        }
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            return base.VisitParameter(node);
+        }
+
+        protected override Expression VisitConstant(ConstantExpression node)
+        {
+            return base.VisitConstant(node);
+        }
+    }
+}
