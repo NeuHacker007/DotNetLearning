1d60b42b1966dfc4eb903db76f6c64bd28275b53
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-08-25 10:03:31 -0400

    basic data structure

--- BurtZhang/BasicCsharp/StructSpecialType/MyStructureDemo/MyStructure/CollectionDemo.cs
+++ BurtZhang/BasicCsharp/StructSpecialType/MyStructureDemo/MyStructure/CollectionDemo.cs
@@ -101,6 +101,133 @@ namespace MyStructure
                     stack.Push("Five");
                 }
             }
+
+            {
+                //SET 集合
+                
+                {
+                    //HashSet: hash分布， 元素间没有关系，动态增加容量，可以去重
+                    //统计用户 IP 交差并补 -- 二次好友/间接关注/共同好友/粉丝合集
+                    Console.WriteLine("*******************HashSet********************");
+                    HashSet<string> hash = new HashSet<string>();
+
+                    hash.Add("123");
+                    hash.Add("456");
+                    hash.Add("789");
+                    hash.Add("987");
+                    hash.Add("765");
+                    hash.Add("543");
+                    hash.Add("321");
+                    hash.Add("321");
+                    hash.Add("321");
+                    hash.Add("321");
+                    {
+                        HashSet<string> hash1 = new HashSet<string>();
+                        hash1.Add("123");
+                        hash1.Add("789");
+                        hash1.Add("111");
+
+                        hash1.SymmetricExceptWith(hash); // 补
+                        hash1.UnionWith(hash); // 并
+                        hash1.ExceptWith(hash); // 差
+                        hash1.IntersectWith(hash); // 交
+                    }
+                }
+
+                {
+                    //排序集合 去重且排序
+                    // 统计排名 -- 每统计一个就丢进去集合
+                    Console.WriteLine("*********************SortedList*******************");
+
+                    SortedSet<string> sortedSet = new SortedSet<string>();
+
+                   sortedSet.Add("123");
+                   sortedSet.Add("456");
+                   sortedSet.Add("789");
+                   sortedSet.Add("987");
+                   sortedSet.Add("765");
+                   sortedSet.Add("543");
+                   sortedSet.Add("321");
+                   sortedSet.Add("321");
+                   sortedSet.Add("321");
+                   sortedSet.Add("321");
+
+                }
+            }
+            {
+                // HashTable key-value 不定长 拿着key计算一个地址，然后放入key-value
+                // object - 装箱拆箱 如果不同的key得到相同的地址，第二个在前面地址+1
+                // 查找的时候，如果地址对应数据的key不对，那就+1 查找
+                // 浪费空间，hashtable是基于数组实现的
+                // 查找数据 一次定位；增删 一次定位 增删改查都很快
+                // 数据太多，重复定位，效率就下去了。
+                {
+                    Console.WriteLine("*************************Hash Table***************************");
+
+                    Hashtable table = new Hashtable();
+                    table.Add("123","456");
+                    table[234] = 456;
+                    table["ivan"] = 456;
+
+                    Hashtable.Synchronized(table);// 线程安全
+                }
+
+                {
+                    //字典 泛型 key-value 增删改查都很快，有序
+                    Console.WriteLine("********************************Dictionary***************************");
+
+                    Dictionary<int, string> dic = new Dictionary<int, string>();
+                    dic.Add(1, "haha");
+                }
+                {
+                    Console.WriteLine("******************Sorted Dictionary*********************");
+
+                    SortedDictionary<int, string> dic = new SortedDictionary<int, string>();
+
+                    dic.Add(1, "Ivan");
+                    dic.Add(3, "Ivan1");
+                    dic.Add(2, "Ivan2");
+                }
+                {
+                    Console.WriteLine("***************Sort List*******************");
+
+                    SortedList sortedList = new SortedList();
+
+
+                    sortedList.Add("First", "Hello");
+                    sortedList.Add("Second", "Hello");
+                    //sortedList.Add("Second", "Hello"); // 不能重复key
+
+                }
+
+                {
+                    List<string> fruit = new List<string>()
+                    {
+                        "apple",
+                        "apple",
+                        "apple",
+                        "apple",
+                        "apple",
+                        "apple",
+                        "apple"
+                    };
+                                     
+                    IEnumerable<string> query = fruit.Where(fruit => fruit.Length > 6); // where 中是传入的委托
+
+                    foreach (var item in query) // 遍历才会去查询比较 迭代器 yield
+                    {
+                        
+                    }
+
+                    IQueryable<string> queryable = fruit.AsQueryable<string>().Where(s => s.Length > 5);// where 中传入的是表达式目录树
+
+                    foreach (var item in queryable) // 表达式目录树的解析， 延迟到遍历时候才去执行， EF的延迟查询
+                    {
+                        
+                    }
+                }
+               
+            }
           
 
         }
