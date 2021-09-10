8e503c68faaf273191b4992c657d85c4c736b511
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-09-10 10:23:15 -0400

    New lottery program init

---
+++ BurtZhang/BasicCsharp/MultiThread/MyAsyncThread/LotteryDemo/frmSSQ.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace LotteryDemo
+{
+    partial class frmSSQ
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbResult = new System.Windows.Forms.GroupBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.lblRed1 = new System.Windows.Forms.Label();
+            this.lblRed2 = new System.Windows.Forms.Label();
+            this.lblRed3 = new System.Windows.Forms.Label();
+            this.lblRed4 = new System.Windows.Forms.Label();
+            this.lblRed5 = new System.Windows.Forms.Label();
+            this.lblRed6 = new System.Windows.Forms.Label();
+            this.lblBlue1 = new System.Windows.Forms.Label();
+            this.gbResult.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbResult
+            // 
+            this.gbResult.Controls.Add(this.lblBlue1);
+            this.gbResult.Controls.Add(this.lblRed6);
+            this.gbResult.Controls.Add(this.lblRed5);
+            this.gbResult.Controls.Add(this.lblRed4);
+            this.gbResult.Controls.Add(this.lblRed3);
+            this.gbResult.Controls.Add(this.lblRed2);
+            this.gbResult.Controls.Add(this.lblRed1);
+            this.gbResult.Location = new System.Drawing.Point(96, 95);
+            this.gbResult.Name = "gbResult";
+            this.gbResult.Size = new System.Drawing.Size(585, 174);
+            this.gbResult.TabIndex = 0;
+            this.gbResult.TabStop = false;
+            this.gbResult.Text = "双色球结果区";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(133, 373);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(517, 373);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(75, 23);
+            this.btnEnd.TabIndex = 1;
+            this.btnEnd.Text = "End";
+            this.btnEnd.UseMnemonic = false;
+            this.btnEnd.UseVisualStyleBackColor = true;
+            // 
+            // lblRed1
+            // 
+            this.lblRed1.AutoSize = true;
+            this.lblRed1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRed1.ForeColor = System.Drawing.Color.Red;
+            this.lblRed1.Location = new System.Drawing.Point(34, 93);
+            this.lblRed1.Name = "lblRed1";
+            this.lblRed1.Size = new System.Drawing.Size(27, 20);
+            this.lblRed1.TabIndex = 2;
+            this.lblRed1.Text = "00";
+            // 
+            // lblRed2
+            // 
+            this.lblRed2.AutoSize = true;
+            this.lblRed2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRed2.ForeColor = System.Drawing.Color.Red;
+            this.lblRed2.Location = new System.Drawing.Point(103, 93);
+            this.lblRed2.Name = "lblRed2";
+            this.lblRed2.Size = new System.Drawing.Size(27, 20);
+            this.lblRed2.TabIndex = 3;
+            this.lblRed2.Text = "00";
+            // 
+            // lblRed3
+            // 
+            this.lblRed3.AutoSize = true;
+            this.lblRed3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRed3.ForeColor = System.Drawing.Color.Red;
+            this.lblRed3.Location = new System.Drawing.Point(168, 93);
+            this.lblRed3.Name = "lblRed3";
+            this.lblRed3.Size = new System.Drawing.Size(27, 20);
+            this.lblRed3.TabIndex = 4;
+            this.lblRed3.Text = "00";
+            // 
+            // lblRed4
+            // 
+            this.lblRed4.AutoSize = true;
+            this.lblRed4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRed4.ForeColor = System.Drawing.Color.Red;
+            this.lblRed4.Location = new System.Drawing.Point(246, 93);
+            this.lblRed4.Name = "lblRed4";
+            this.lblRed4.Size = new System.Drawing.Size(27, 20);
+            this.lblRed4.TabIndex = 5;
+            this.lblRed4.Text = "00";
+            // 
+            // lblRed5
+            // 
+            this.lblRed5.AutoSize = true;
+            this.lblRed5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRed5.ForeColor = System.Drawing.Color.Red;
+            this.lblRed5.Location = new System.Drawing.Point(324, 93);
+            this.lblRed5.Name = "lblRed5";
+            this.lblRed5.Size = new System.Drawing.Size(27, 20);
+            this.lblRed5.TabIndex = 6;
+            this.lblRed5.Text = "00";
+            // 
+            // lblRed6
+            // 
+            this.lblRed6.AutoSize = true;
+            this.lblRed6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRed6.ForeColor = System.Drawing.Color.Red;
+            this.lblRed6.Location = new System.Drawing.Point(400, 93);
+            this.lblRed6.Name = "lblRed6";
+            this.lblRed6.Size = new System.Drawing.Size(27, 20);
+            this.lblRed6.TabIndex = 7;
+            this.lblRed6.Text = "00";
+            // 
+            // lblBlue1
+            // 
+            this.lblBlue1.AutoSize = true;
+            this.lblBlue1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBlue1.ForeColor = System.Drawing.Color.Blue;
+            this.lblBlue1.Location = new System.Drawing.Point(503, 93);
+            this.lblBlue1.Name = "lblBlue1";
+            this.lblBlue1.Size = new System.Drawing.Size(27, 20);
+            this.lblBlue1.TabIndex = 8;
+            this.lblBlue1.Text = "00";
+            // 
+            // frmSSQ
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbResult);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnEnd);
+            this.Name = "frmSSQ";
+            this.Text = "双色球";
+            this.gbResult.ResumeLayout(false);
+            this.gbResult.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbResult;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Label lblBlue1;
+        private System.Windows.Forms.Label lblRed6;
+        private System.Windows.Forms.Label lblRed5;
+        private System.Windows.Forms.Label lblRed4;
+        private System.Windows.Forms.Label lblRed3;
+        private System.Windows.Forms.Label lblRed2;
+        private System.Windows.Forms.Label lblRed1;
+    }
+}
+
