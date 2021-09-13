7dae3fae1a6426ae9267448ff10cb3574ea1f968
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-09-13 13:26:28 -0400

    AOP init

---
+++ BurtZhang/BasicCsharp/AOP/MyAOP/MyAOP/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyAOP
+{
+    /// <summary>
+    /// 1. AOP 面向切面编程
+    /// 2. 静态代理实现AOP
+    /// 3. 动态代理实现AOP
+    /// 4. Unity, MVC中的AOP
+    ///
+    /// AOP：面向切面编程 编程思想
+    /// OOP: 一切皆对象，对象交互组成功能，功能叠加组成模块，模块叠加组成系统
+    ///     面向对象是非常适合做大系统
+    ///
+    ///     应对需求变化扩展的时候，我们会有较大影响:
+    ///     面对对象是静态的: 任何需求的细微变化都可能导致比较大的影响 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
