ccede0953e3aafd292aa1d8e23f926f2daa11a6d
Ivan Zhang <yf.eva.yifan@gmail.com>
2021-09-21 14:57:01 -0400

    Project Init

---
+++ BurtZhang/DesignPattern/DP/DesignPatternDemo/DesignPattern/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DesignPattern
+{
+    /// <summary>
+    /// 设计模式： 面向对象语言开发过程中，遇到的种种问题和场景，提出的解决方案和思路，沉淀总结
+    ///           套路 具体的套路
+    ///
+    /// 1. 创建型设计模式： 关注对象的创建
+    ///     * 单例模式 : 把对象的创建权限关闭，提供一个方法，起到对象重用
+    ///     * 原型模式 : 把对象的创建权限关闭，提供一个方法，提供全新的对象
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                
+            }
+        }
+    }
+}
