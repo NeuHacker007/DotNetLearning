07405bcaae8d588673f6aedeada3afad3292c1ca
Ivan Zhang <yf.eva.yifan@gmail.com>
2022-03-03 20:32:20 -0500

    ofType demo

---
+++ LinqDemo/LinqDemo/OfTypeDemo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDemo
+{
+    public class OfTypeDemo
+    {
+        public static void Demo1()
+        {
+            List<object> dataSource = new List<object>()
+            {
+                "Tom", "Mary", 50, "Prince", "Jack", 10, 20, 30, 40, "James"
+            };
+
+            var intsMethodFormat = dataSource.OfType<int>().ToList();
+            var intsQueryFormat = (from item in dataSource
+                                  where item is int
+                                  select item).ToList();
+            var strs = dataSource.OfType<string>().ToList();
+            var strsQueryFormat = (from item in dataSource
+                                   where item is string
+                                   select item).ToList();
+
+            foreach (var item in intsMethodFormat)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("*************************************");
+            foreach (var item in strs)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+
+    }
+}
--- LinqDemo/LinqDemo/Program.cs
+++ LinqDemo/LinqDemo/Program.cs
@@ -6,7 +6,7 @@ namespace LinqDemo
     {
         static void Main(string[] args)
         {
-            SelectManyDemo.Demo5();
+            OfTypeDemo.Demo1();
         }
     }
 }
