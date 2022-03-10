90359be71e956092a9a9b7def40692973b78689d
Ivan Zhang <yf.eva.yifan@gmail.com>
2022-03-10 09:10:17 -0500

    Test the Static method with Test the DateTime

---
+++ UnitTestDemo/TestNinja.UnitTest/Fundamental/DateHelperTests.cs
@@ -0,0 +1,25 @@
+﻿namespace TestNinja.UnitTest.Fundamental
+{
+    [TestFixture]
+    public class DateHelperTests
+    {
+        [Test]
+        public void FirstOfNextMonth_DateMonthIs12_ShouldReturnTheFirstDayOfNextYear()
+        {
+            var inputDateTime = new DateTime(2021, 12, 1);
+            var expectedResult = new DateTime(2022, 1, 1);
+            var result = DateHelper.FirstOfNextMonth(inputDateTime);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+        [Test]
+        public void FirstOfNextMonth_DateMonthIsNot12_ShouldReturnTheNextMonthDateWithinTheSameYearAndSameDay()
+        {
+            var inputDateTime = new DateTime(2021, 11, 1);
+            var expectedResult = new DateTime(2021, 12, 1);
+            var result = DateHelper.FirstOfNextMonth(inputDateTime);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
