88b0c0b3c980d28f91ceb42a8457262dcc9b5974
Ivan Zhang <yf.eva.yifan@gmail.com>
2022-03-09 22:05:48 -0500

    Test Array Test return value

---
+++ UnitTestDemo/TestNinja.UnitTest/Fundamental/MathTests.cs
@@ -0,0 +1,52 @@
+﻿namespace TestNinja.UnitTest.Fundamental
+{
+    [TestFixture]
+    public class MathTests
+    {
+        private fundamental.Math _math;
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Fundamentals.Math();
+        }
+        [Test]
+        [TestCase(1, 3, 4)]
+        public void Add_WhenCalled_ReturnTheSumOfArgs(int a, int b, int expectedResult)
+        {
+            var result = _math.Add(a, b);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase(1, 1, 1)]
+        [TestCase(1, 3, 3)]
+        [TestCase(2, 1, 2)]
+        public void Max_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
+        {
+            var result = _math.Max(a, b);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void GetOddNumbers_LimitIsGreaterThanZero_ReturnOddNumbersUpToLimit()
+        {
+            var result = _math.GetOddNumbers(5);
+
+            //Assert.That(result, Does.Contain(1));
+            //Assert.That(result, Does.Contain(3));
+            //Assert.That(result, Does.Contain(5));
+
+            Assert.That(result, Is.EquivalentTo(new int[] { 1, 3, 5 }));
+        }
+
+        [Test]
+        public void GetOddNumbers_LimitIsLessThanZero_ReturnEmpty()
+        {
+            var result = _math.GetOddNumbers(-5);
+
+            Assert.That(result, Is.Empty);
+        }
+    }
+}
--- UnitTestDemo/TestNinja.UnitTest/GlobalNameSpaces.cs
+++ UnitTestDemo/TestNinja.UnitTest/GlobalNameSpaces.cs
@@ -1,5 +1,6 @@
 ﻿global using System;
 global using NUnit.Framework;
+global using fundamental=TestNinja.Fundamentals;
 global using TestNinja.Fundamentals;
 
 
