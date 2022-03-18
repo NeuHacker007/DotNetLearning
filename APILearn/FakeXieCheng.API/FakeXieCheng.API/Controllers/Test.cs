903cae7238327228e02e5012c4ccdd8d46934461
Ivan Zhang <yf.eva.yifan@gmail.com>
2022-03-18 15:09:20 -0400

    Initial Project

---
+++ APILearn/FakeXieCheng.API/FakeXieCheng.API/Controllers/Test.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace FakeXieCheng.API.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class Test: ControllerBase
+    {
+
+        public string Test1()
+        {
+            return "Hello";
+        }
+
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,15 +1,60 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+
 namespace ConsoleApp1
 {
   
     internal class Program
     {
       
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
 
+             //testAsnc();
+            Student student1 = new Student {
+                
+                Name = "Ivan"
+                };
+
+            var student2 = student1;
+            
+            student1 = null;
+            Console.WriteLine($"{student2.Name}");
+           // Console.WriteLine($"{student1.Name}");
+
+             
+            Console.WriteLine("Hello World");
+        }
+
+        public class Student
+        {
+            public string Name { get; set; }
+        }
+
+        public static async void testAsnc()
+        {
+            var factory = new TaskFactory();
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            var task = factory.StartNew(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            });
+            await task;
+
+            Console.WriteLine("Test");
+        } 
+
+        public static async int test2Async()
+        {
+            
+
+
+            
+            return 1;
         }
 
     }
