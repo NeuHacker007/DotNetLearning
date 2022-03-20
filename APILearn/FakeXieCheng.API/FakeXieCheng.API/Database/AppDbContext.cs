5222e9d85951152cf927360a2c7636b0ad6987e7
Ivan Zhang <yf.eva.yifan@gmail.com>
2022-03-19 23:34:27 -0400

    Ch3.9 Load all the TouristRoutes and TouristRoutePictures Test Data

--- APILearn/FakeXieCheng.API/FakeXieCheng.API/Database/AppDbContext.cs
+++ APILearn/FakeXieCheng.API/FakeXieCheng.API/Database/AppDbContext.cs
@@ -1,16 +1,47 @@
 ﻿using FakeXieCheng.API.Models;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+//using System.Text.Json;
+using Newtonsoft.Json;
 
 namespace FakeXieCheng.API.Database
 {
-    public class AppDbContext: DbContext
+    public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
         }
 
-        public DbSet<TouristRoute> TouristRoutes {get; set;}
-        public DbSet<TouristRoutePicture> TouristRoutePictures {get;set;}
+        public DbSet<TouristRoute> TouristRoutes { get; set; }
+        public DbSet<TouristRoutePicture> TouristRoutePictures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<TouristRoute>().HasData(new TouristRoute()
+            //{
+            //    Id = Guid.NewGuid(),
+            //    Title = "ceshititle",
+            //    Description = "shuoming",
+            //    OriginalPrice = 0,
+            //    CreateTime = DateTime.UtcNow
+
+            //});
+            //if (System.Diagnostics.Debugger.IsAttached == false)
+            //{
+            //    System.Diagnostics.Debugger.Launch();
+            //}
+            string touristRouteJsonData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Database\touristRoutesMockData.json");
+            IList<TouristRoute> touristRoutes =  JsonConvert.DeserializeObject<IList<TouristRoute>>(touristRouteJsonData);
+            modelBuilder.Entity<TouristRoute>().HasData(touristRoutes);
+
+            var touristRoutePictureJsonData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Database\touristRoutePicturesMockData.json");
+            IList<TouristRoutePicture> touristRoutePictures = JsonConvert.DeserializeObject<IList<TouristRoutePicture>>(touristRoutePictureJsonData);
+            modelBuilder.Entity<TouristRoutePicture>().HasData(touristRoutePictures);
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
