8af339683ac44fee538841a72de0b25c12a5a434
Ivan Zhang <yf.eva.yifan@gmail.com>
2022-03-19 21:49:17 -0400

    ch 3.3 repository

---
+++ APILearn/FakeXieCheng.API/FakeXieCheng.API/Controllers/TouristRoutesController.cs
@@ -0,0 +1,25 @@
+﻿using FakeXieCheng.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FakeXieCheng.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TouristRoutesController : ControllerBase
+    {
+        private readonly ITouristRouteRepository _touristRouteRepository;
+
+        public TouristRoutesController(ITouristRouteRepository touristRouteRepository)
+        {
+            _touristRouteRepository = touristRouteRepository;
+        }
+
+        public IActionResult GetRouristRoutes()
+        {
+            var routes = _touristRouteRepository.GetTouristRoutes();
+
+            return Ok(routes);
+        }
+    }
+}
