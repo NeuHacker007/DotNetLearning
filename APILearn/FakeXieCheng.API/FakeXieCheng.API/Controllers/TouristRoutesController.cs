bf38e45b8ac2c46f1eefa66211ffad1349ced27e
Ivan Zhang <yf.eva.yifan@gmail.com>
2022-03-21 13:18:52 -0400

    Ch5.3 Return correct HTTP status code

--- APILearn/FakeXieCheng.API/FakeXieCheng.API/Controllers/TouristRoutesController.cs
+++ APILearn/FakeXieCheng.API/FakeXieCheng.API/Controllers/TouristRoutesController.cs
@@ -1,6 +1,8 @@
 ﻿using FakeXieCheng.API.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System;
 
 namespace FakeXieCheng.API.Controllers
 {
@@ -15,11 +17,27 @@ namespace FakeXieCheng.API.Controllers
             _touristRouteRepository = touristRouteRepository;
         }
 
+        [HttpGet]
         public IActionResult GetRouristRoutes()
         {
-            var routes = _touristRouteRepository.GetTouristRoutes();
+            var touristRoutesFromRepo = _touristRouteRepository.GetTouristRoutes();
+            if (touristRoutesFromRepo == null 
+                || touristRoutesFromRepo.Count() <= 0 )
+            {
+                return NotFound("没有旅游路线");
+            }
+            return Ok(touristRoutesFromRepo);
+        }
 
-            return Ok(routes);
+        [HttpGet("{touristRouteId:Guid}")]
+        public IActionResult GetTouristRoutesById(Guid touristRouteId)
+        {
+            var touristRouteFromRepo = _touristRouteRepository.GetTouristRoute(touristRouteId);
+            if (touristRouteFromRepo == null)
+            {
+                return NotFound($"旅游路线{touristRouteId}找不到");
+            }
+            return Ok(touristRouteFromRepo);
         }
     }
 }
