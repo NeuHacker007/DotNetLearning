f9f60dec0df54fb37864f45b44a175177c9b3ffa
Ivan Zhang <yf.eva.yifan@gmail.com>
2022-03-23 13:15:18 -0400

    ch8.3 add comments

--- APILearn/FakeXieCheng.API/FakeXieCheng.API/Dtos/TouristRouteForUpdateDto.cs
+++ APILearn/FakeXieCheng.API/FakeXieCheng.API/Dtos/TouristRouteForUpdateDto.cs
@@ -11,6 +11,11 @@ namespace FakeXieCheng.API.Dtos
         
         [Required(ErrorMessage = "Description 不可为空")]
         [MaxLength(1500)]
+        /*
+         调用使用new来处理的方法，子类的引用调用执行子类的方法，基类的引用调用执行基类的方法。
+         而对于使用override修饰的方法来说，不管是子类还是基类都只会执行子类的方法。
+         
+         */
         public override string Description { get; set; }
 
        
