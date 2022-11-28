34d4e372a464df082fabe96a01f5a80394e1e8b7
Ivan Zhang <yf.eva.yifan@gmail.com>
2022-11-28 17:27:59 -0500

    Environment Config

---
+++ NetCoreLearning/Projects/GeekTime/SelfLearn/GeekTimeSelfLearn/EnvVariableConfig/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace EnvVariableConfig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+
+            //builder.AddEnvironmentVariables();
+
+            //var configRoot = builder.Build();
+
+            //Console.WriteLine($"Ivan_Key1: {configRoot["Ivan_Key1"]}");
+            //var section = configRoot.GetSection("section1");
+            //Console.WriteLine($"Section1__Key3: {section["Key3"]}");
+
+            //var section2 = configRoot.GetSection("section1:section2");
+
+            //Console.WriteLine($"Section1__Section2__Key4: {section2["Key4"]}");
+            //Console.WriteLine($"Ivan_Key1: {configRoot["Ivan_Key1"]}");
+
+            builder.AddEnvironmentVariables("Ivan_");
+
+            var configRoot = builder.Build();
+
+            Console.WriteLine($"Ivan_Key1: {configRoot["key1"]}");
+            Console.WriteLine($"Key2: {configRoot["key2"]}");
+        }
+    }
+}
