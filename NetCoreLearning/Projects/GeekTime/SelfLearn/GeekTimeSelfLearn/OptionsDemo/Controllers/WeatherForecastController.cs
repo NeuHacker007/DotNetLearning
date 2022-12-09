689cb45debc61795e7d3a3423c17f9ff970e45ea
Ivan Zhang <yf.eva.yifan@gmail.com>
2022-12-09 08:34:08 -0500

    Options configuration

---
+++ NetCoreLearning/Projects/GeekTime/SelfLearn/GeekTimeSelfLearn/OptionsDemo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OptionsDemo.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OptionsDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public int Get([FromServices] IOrderService orderService)
+        {
+            Console.WriteLine($"orderService.ShowMaxOrderCount: {orderService.ShowMaxOrderCount()}");
+            return 1;
+        }
+    }
+}
---
+++ NetCoreLearning/Projects/GeekTime/SelfLearn/GeekTimeSelfLearn/OptionsDemo/Services/OrderService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace OptionsDemo.Services
+{
+    public interface IOrderService
+    {
+        int ShowMaxOrderCount();
+    }
+
+    public class OrderService : IOrderService
+    {
+        private IOptions<OrderServiceOptions> _options;
+        public OrderService(IOptions<OrderServiceOptions> options)
+        {
+            _options = options;
+        }
+
+        public int ShowMaxOrderCount()
+        {
+            return _options.Value.MaxOrderCount;
+        }
+    }
+
+    public class OrderServiceOptions
+    {
+        public int MaxOrderCount { get; set; } = 100;
+    }
+}
