8c6bf103b9ca5d6ffdab8b9df40b74b8bba7d980
Ivan Zhang <yf.eva.yifan@gmail.com>
2022-12-18 22:16:57 -0500

    Logging scope

---
+++ NetCoreLearning/Projects/GeekTime/SelfLearn/GeekTimeSelfLearn/LoggingScopeDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace LoggingScopeDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var configBuilder = new ConfigurationBuilder();
+            configBuilder.AddCommandLine(args);
+            configBuilder.AddJsonFile("appsettings.json", reloadOnChange: true, optional: false);
+            var config = configBuilder.Build();
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton(c => config);
+
+            services.AddLogging(builder =>
+            {
+                builder.AddConfiguration(config.GetSection("logging"));
+                builder.AddConsole();
+                builder.AddDebug();
+            });
+
+            IServiceProvider service = services.BuildServiceProvider();
+
+            var logger = service.GetService<ILogger<Program>>();
+
+            using(logger.BeginScope("ScopeId: {ScopeId}", Guid.NewGuid()))
+            {
+                logger.LogInformation("Info");
+                logger.LogError("Error");
+                logger.LogTrace("Trace");
+            }
+
+        }
+    }
+}
